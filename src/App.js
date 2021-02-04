0fcd384e78f2bd9925fb801d4f42225e987f69e6
= <=bshin132@gmail.com>
2021-02-03 16:45:26 -0800

    Need help with SearchBar comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Main from './pages/index';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import Search from './search.png';
+
+const Container = styled.input`
+min-width:314px;
+min-height:39px;
+background-color:#E5E5E5;
+border:none;
+border-radius:100px;
+font-size:18px;
+font-color:#919191;
+display:flex;
+flex-direction:row;
+`;
+
+const IconImg = styled.img`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 16px;
+height: 17px;
+`;
+
+const SearchPNG = {
+  src: Search
+}
 
 const SearchBar = ({}) => {
-  return <></>;
+  return <Container placeholder="Search for a food">
+    <IconImg source={SearchPNG}/>
+  </Container>
 };
 
 SearchBar.defaultProps = {};
