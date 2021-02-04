95cf9d9186a2ba746920d274b0574c71c7866062
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2021-02-03 16:57:00 -0800

    fix images thing

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,35 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-import Search from './search.png';
 
-const Container = styled.input`
-min-width:314px;
-min-height:39px;
-background-color:#E5E5E5;
-border:none;
-border-radius:100px;
-font-size:18px;
-font-color:#919191;
-display:flex;
-flex-direction:row;
+const Container = styled.div`
+  position: relative;
 `;
 
 const IconImg = styled.img`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 16px;
-height: 17px;
+  position: absolute;
+  top: 11px;
+  left: 10px;
+  width: 16px;
+  height: 17px;
 `;
 
-const SearchPNG = {
-  src: Search
-}
+const Input = styled.input`
+  min-width: 314px;
+  min-height: 39px;
+  background-color: #e5e5e5;
+  border: none;
+  border-radius: 100px;
+  font-size: 18px;
+  color: #919191;
+  display: flex;
+  padding-left: 10px;
+  flex-direction: row;
+`;
 
 const SearchBar = ({}) => {
-  return <Container placeholder="Search for a food">
-    <IconImg source={SearchPNG}/>
-  </Container>
+  return (
+    <Container placeholder="Search for a food">
+      <Input></Input>
+      <IconImg src="./search.png" />
+    </Container>
+  );
 };
 
 SearchBar.defaultProps = {};
