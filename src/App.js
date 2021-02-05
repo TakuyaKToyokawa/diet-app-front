1e6cc7f5339168424ece1804695b995d157d9246
Pooneh Ashja <poone.ashja@gmail.com>
2021-02-04 21:27:46 -0800

    Dairy page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.scss';
-import Main from './pages/index';
-import Food from './pages/food';
-import SearchBar from './components/SearchBar';
-import MacronutrientsInfo from './components/MacronutrientsInfo';
+import "./App.scss";
+import Main from "./pages/index";
+import Dairy from "./pages/Dairy";
 
 function App() {
   return (
     <div className="App">
-      <Food></Food>
+      <Dairy></Dairy>
     </div>
   );
 }
