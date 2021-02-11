dc7bc3be87e93f46aa1dcb3af1cbc52bf5857d98
TakuyaToyokawa <takuya.k.toyokawa@gmail.com>
2021-02-11 00:46:13 -0800

    change database structure

--- src/database/sampleFoods.js
+++ src/database/sampleFoods.js
@@ -1,57 +1,57 @@
 
 export const foods = [
   {
-    category: "Dairy",
-    name: "Milk",
-    calories: 100,
-    carbs: 123,
-    protein: 200,
-    fat: 10
+    "category": "Dairy",
+    "name": "Milk",
+    "calories": 100,
+    "carbs": 123,
+    "protein": 200,
+    "fat": 10
   },
 
   {
-    category: "Fruit",
-    name: "Strawberry",
-    calories: 400,
-    carbs: 223,
-    protein: 200,
-    fat: 10
+    "category": "Fruit",
+    "name": "Strawberry",
+    "calories": 400,
+    "carbs": 223,
+    "protein": 200,
+    "fat": 10
   },
 
   {
-    category: "Meat",
-    name: "Egg",
-    calories: 100,
-    carbs: 123,
-    protein: 200,
-    fat: 10
+    "category": "Meat",
+    "name": "Egg",
+    "calories": 100,
+    "carbs": 123,
+    "protein": 200,
+    "fat": 10
   },
 
   {
-    category: "Vegetable",
-    name: "Celery",
-    calories: 500,
-    carbs: 223,
-    protein: 200,
-    fat: 10
+    "category": "Vegetable",
+    "name": "Celery",
+    "calories": 500,
+    "carbs": 223,
+    "protein": 200,
+    "fat": 10
   },
 
   {
-    category: "Grain",
-    name: "Bread",
-    calories: 500,
-    carbs: 223,
-    protein: 200,
-    fat: 10
+    "category": "Grain",
+    "name": "Bread",
+    "calories": 500,
+    "carbs": 223,
+    "protein": 200,
+    "fat": 10
   },
 
   {
-    category: "Sweet",
-    name: "Pancake",
-    calories: 500,
-    carbs: 223,
-    protein: 200,
-    fat: 10
+    "category": "Sweet",
+    "name": "Pancake",
+    "calories": 500,
+    "carbs": 223,
+    "protein": 200,
+    "fat": 10
   },
 ];
 
